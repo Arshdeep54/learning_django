4bcb05936cbc3236ad63ed91888d8924501d4255
Arshdeep <balarsh535@gmail.com>
2023-12-11 12:19:44 +0530

    learnt about authentication not coded but

--- learnbackend/settings.py
+++ learnbackend/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "debug_toolbar",
     "store",
     "tags",
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -145,6 +146,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "COERCE_DECIMAL_TO_STRING": False,
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
-    # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
+# AUTH_USER_MODEL = "core.User"
